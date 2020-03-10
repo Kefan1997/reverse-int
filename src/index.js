fd19dcc2f9c15fbf6b65a7c8624d3aef27f54dfb
Oleg Rashinskiy <oleg.rashinskiy@gmail.com>
2020-03-10 18:12:31 +0300

    init: Reverse-Int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let stringNumber = String(Math.abs(n));
+    console.log(stringNumber);
+    let arrayNumber = [];
+
+    for (let i = 0; i < stringNumber.length; i++) {
+      arrayNumber.push(stringNumber[i]);
+    }
+    arrayNumber.reverse();
+    let str = arrayNumber.join("");
+    return str;
 }
